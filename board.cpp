610b0fca66a9b85044b905c9beb49f894a17f9b6
Muhammad Farjad <86410882+BeastMasterGrinder@users.noreply.github.com>
2023-04-02 22:57:59 +0500

    Changed Board and box sizes

--- board.cpp
+++ board.cpp
@@ -1,29 +1,48 @@
-#include "board.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <array>
 
-Board::Board(Color col1, Color col2){
-    load(col1, col2);
-}
+using namespace sf;
 
-// Member function that sets Board stuff, can choose square colors in parameters
-void Board::load(sf::Color col1, sf::Color col2){
-    for(int i=0; i<8;i++){
+class Board : public sf::Drawable{
 
-        bool tmpColor = ((i % 2)==0)?true:false;
-
-        for(int j=0; j<8;j++){
-
-            m_boardSquares[j + (i * 8)].setPosition(sf::Vector2f( j*64.f , i*64.f ));
-            m_boardSquares[j + (i * 8)].setSize(sf::Vector2f(64.f, 64.f));
-            m_boardSquares[j + (i * 8)].setFillColor(tmpColor ? col1 : col2);
-            
-            tmpColor = !tmpColor;
+    std::array<RectangleShape, 400> m_boardSquares;
+    
+    // Draw class on SFML Window
+    virtual void draw(RenderTarget& target, RenderStates states) const{
+        for(int i=0;i<400;i++){
+            target.draw(m_boardSquares[i]);
         }
     }
-}
 
-// Draw class on SFML Window
-void Board::draw(sf::RenderTarget& target, sf::RenderStates states) const{
-    for(int i=0;i<64;i++){
-        target.draw(m_boardSquares[i]);
+public:
+    //if no color is added then they have pre defined colors
+    Board(Color col1 = Color (122, 235, 52), Color col2 = Color (138, 51, 8)){
+        load(col1, col2);
     }
-}
+    
+    //if no color is added then they have pre defined colors
+    // Member function that sets Board stuff, can choose square colors in parameters
+    //assuming the board is 15*15
+    void load(Color col1 = Color (122, 235, 52), Color col2 = Color (138, 51, 8)){
+        float boxSize = 400.f/15.f;
+        //20 is the number of boxes and 400/15 is the size of the box
+
+        for(int i=0; i<20;i++){
+            bool tmpColor = ((i % 2)==0)?true:false;
+
+            for(int j=0; j<20;j++){
+                
+                m_boardSquares[j + (i * 20)].setPosition(sf::Vector2f( j*boxSize , i*boxSize ));
+                m_boardSquares[j + (i * 20)].setSize(sf::Vector2f(boxSize, boxSize));
+                m_boardSquares[j + (i * 20)].setFillColor(tmpColor ? col1 : col2);
+                
+                tmpColor = !tmpColor;
+            }
+        }
+    }
+
+
+
+};
+
