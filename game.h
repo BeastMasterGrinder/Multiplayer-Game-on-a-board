9933151a64b416514d3ecd9d2b60e30993eacb97
Muhammad Farjad <86410882+BeastMasterGrinder@users.noreply.github.com>
2023-04-02 22:45:46 +0500

    Players from Game
    Game.startgame() and other functionalities
    Players are made in the Game class

--- game.h
+++ game.h
@@ -3,17 +3,56 @@
 #include<vector>
 #include<iostream>
 #include"board.h"
-#include"player.h"
+#include"players.cpp"
 
 class Game: public sf::Drawable {
+public:
     Board board;
 
     sf::Font font;
+    // std::vector<PlayerData> players;
+    PlayerData * player1_data;
+    PlayerData * player2_data;
     
+
+    Game(sf::Color bordCol1, sf::Color bordCol2);
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-
-    public:
-    Game(sf::Color bordCol1, sf::Color bordCol2){
-
-    }
+    void startGame();
 };
+
+Game::Game(sf::Color bordCol1, sf::Color bordCol2){
+    board = Board(bordCol1, bordCol2);
+    
+    //20 blocks 
+    player1_data = new PlayerData(0, true, 0, 0);
+    player2_data = new PlayerData(19, false, 19, 0);
+
+
+    // PlayerData player1;
+    // PlayerData player2;
+
+    // players.push_back(player1);
+    // players.push_back(player2);
+}
+
+void Game::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+    target.draw(board);
+    target.draw(*player1_data);
+    target.draw(*player2_data);
+}
+
+void Game::startGame(){
+    sf::RenderWindow window(sf::VideoMode(640, 640), "Multiplayer Game");
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(board);
+        window.draw(*player1_data);
+        window.draw(*player2_data);
+        window.display();
+    }
+}
